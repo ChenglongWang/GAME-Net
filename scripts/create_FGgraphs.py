d209fd4ef54bf193bae6b80c736994dc5fe095a5
SMorandi95 <smorandi@iciq.es>
2022-12-15 17:20:41 +0100

    Added scripts to generate FG and BM-datasets

--- scripts/create_FGgraphs.py
+++ scripts/create_FGgraphs.py
@@ -1,3 +1,5 @@
+"""Convert raw graph samples of the FG-dataset to Pytorch Geometric format."""
+
 import argparse
 import sys
 sys.path.insert(0, "../src")
@@ -11,10 +13,16 @@ if __name__ == "__main__":
     PARSER = argparse.ArgumentParser(description='Convert DFT samples of the FG dataset to graph formats')
     PARSER.add_argument('--data', type=str, dest="data", help='Path to data folder')
     PARSER.add_argument('--tol', type=float, default=0.5, dest="tol", help='Voronoi tolerance')
-    PARSER.add_argument('--second_order_nn', type=bool, default=False, dest="sec_nn", help='Whether to include the 2-hop metal atoms neighbours')
+    PARSER.add_argument('--metal-hops', type=int, default=1, dest="metal_hops", help='Number of metal hops to consider in the graph')
     PARSER.add_argument('--scaling_factor', type=float, default=1.5, dest="sf", help='Scaling factor applied to metal atomic radii')
     ARGS = PARSER.parse_args()
-    graph_settings = {"voronoi_tol":ARGS.tol, "scaling_factor":ARGS.sf, "second_order_nn":ARGS.sec_nn}
+    if ARGS.metal_hops == 1:
+        sec_nn = False
+    elif ARGS.metal_hops == 2:
+        sec_nn = True
+    else: 
+        print("Please choose a valid number of metal hops (1 or 2).")
+    graph_settings = {"voronoi_tol":ARGS.tol, "scaling_factor":ARGS.sf, "second_order_nn": sec_nn}
     id = get_id(graph_settings)
     paths = create_paths(FG_RAW_GROUPS, ARGS.data, id)
     create_graph_datasets(graph_settings, paths)
