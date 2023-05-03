d82625180e1eaffd61ec569772c024fa288fe1ed
oliverlovros <oliverlovros@gmail.com>
2023-05-03 17:15:19 +0200

    Cleaned act_sites.py function

--- new_web/adsurf/functions/act_sites.py
+++ new_web/adsurf/functions/act_sites.py
@@ -6,57 +6,6 @@ from pymatgen.analysis.adsorption import AdsorbateSiteFinder
 from gnn_eads.functions import get_voronoi_neighbourlist
 from pymatgen.io.ase import AseAtomsAdaptor
 
-
-# def get_act_sites(metal_poscar: str) -> dict:
-#     # TODO: Refine this function to find the active sites of different metal facets
-#     """Finds the active sites of a metal surface. These can be ontop, bridge or hollow sites.
-
-#     Parameters
-#     ----------
-#     metal_poscar : str
-#         Path to the POSCAR of the metal surface.
-
-#     Returns
-#     -------
-#     dict
-#         Dictionary with the atom indexes of each active site type.
-#     """
-#     surface = Structure.from_file(metal_poscar)
-#     surf_sites = AdsorbateSiteFinder(surface, selective_dynamics=True)
-#     most_active_sites = surf_sites.find_adsorption_sites()
-
-#     surface_sites = surf_sites.surface_sites
-
-#     act_sites_dict = {}
-#     for site in surface_sites:
-#         distances = np.linalg.norm(surface.cart_coords - site.coords, axis=1)
-#         index = np.argmin(distances)
-#         act_sites_dict[index] = surface[index].coords
-
-#     act_sites_arr = np.array([c for c in act_sites_dict.values()])
-    
-#     active_site_dict = {'ontop':[], 'bridge':[], 'hollow':[]}
-#     for name_site, coords_site in most_active_sites.items():
-#         if name_site == 'all':
-#             continue
-
-#         for coord in coords_site:
-#             distances = np.linalg.norm(act_sites_arr - coord, axis=1)
-#             if name_site != 'hollow':
-#                 index = np.argmin(distances)
-#                 coords_in_surface = act_sites_arr[index]
-#                 idx_surface = [key for key, coord in act_sites_dict.items() if np.all(coord == coords_in_surface)][0]
-
-#                 active_site_dict[name_site].append(idx_surface)
-#             else:
-#                 sorted_arr = np.sort(distances)[0:3]
-#                 for i in sorted_arr:
-#                     coords_in_surface = act_sites_arr[np.where(distances == i)][0]
-#                     idx_surface = [key for key, coord in act_sites_dict.items() if np.all(coord == coords_in_surface)][0]
-#                     active_site_dict[name_site].append(idx_surface)
-
-#     return active_site_dict
-
 def get_act_sites(metal_poscar: str) -> dict:
     # TODO: Refine this function to find the active sites of different metal facets
     """Finds the active sites of a metal surface. These can be ontop, bridge or hollow sites.
@@ -73,7 +22,6 @@ def get_act_sites(metal_poscar: str) -> dict:
     """
     surface = Structure.from_file(metal_poscar)
 
-    sel_dynamics = surface.site_properties
     surface.remove_site_property('selective_dynamics')
     
     surf_sites = AdsorbateSiteFinder(surface, selective_dynamics=True)
